84999e736c13db43ec4b3b6ccf4c5a6e7251023f
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-12-14 12:38:37 +0600

    Создана модель фотографии и комментария

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='photo_images', null=False, blank=False, verbose_name='Фотография', )
+    signature = models.CharField(max_length=20, blank=False, verbose_name='Подпись'),
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата-время создания')
+    likes = models.ManyToManyField(User, related_name='likes', null=True, blank=True)
+    author = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE, verbose_name='Автор')
+
+class Comment(models.Model):
+    text = models.TextField(max_length=2000, blank=False, verbose_name='Комментарий')
+    image = models.ForeignKey('webapp.Photo', related_name='comments', on_delete=models.CASCADE,
+                              verbose_name='Фотография')
+    author = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE, verbose_name='Автор')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата-время создания')
